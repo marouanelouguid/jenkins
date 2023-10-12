8b0a6ed4d6c7c782c26ad81ca1065e719c207ed9
marouanelouguid <marouane@valbray.fr>
2023-10-12 17:12:13 +0100

    hi

---
+++ test001.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Test Page</title>
+</head>
+<body>
+    <h1>Hello, this is a PHP Test Page</h1>
+
+    <?php
+    // PHP code starts here
+    $name = "John Doe";
+    $age = 30;
+
+    echo "My name is " . $name . " and I am " . $age . " years old.";
+
+    // End of PHP code
+    ?>
+
+    <p>This is an HTML paragraph outside of PHP.</p>
+</body>
+</html>
